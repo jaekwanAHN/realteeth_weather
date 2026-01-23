9775898ff0f12e14dbadf91c3643c82f7ea47161
JaeKwan Ahn <52008@naver.com>
2026-01-23 22:25:20 +0900

    chore: 이미지 호스트 허용 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org', // 날씨 아이콘 도메인 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
